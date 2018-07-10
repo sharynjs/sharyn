b15206c926af957d66fbb2c07736cedcea9a3174
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2018-07-10 15:50:43 +0300

    Add peerDependencies to sharyn-cli, use .eslintrc.js for sharyn-config

---
+++ packages/sharyn-config/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  extends: [
+    'airbnb',
+    'plugin:flowtype/recommended',
+    'prettier',
+    'prettier/react',
+    'prettier/flowtype',
+  ],
+  plugins: ['flowtype', 'prettier'],
+  env: {
+    browser: true,
+    jest: true,
+  },
+  rules: {
+    'no-unexpected-multiline': 2,
+    'import/prefer-default-export': 0,
+    'prettier/prettier': 2,
+    'func-names': 0,
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js'],
+      },
+    ],
+  },
+}
