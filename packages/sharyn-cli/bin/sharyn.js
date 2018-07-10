58c58c624da481faaa5b353e6857e613861bfddf
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2018-07-10 15:34:08 +0300

    Add sharyn-config

--- packages/sharyn-cli/bin/sharyn.js
+++ packages/sharyn-cli/bin/sharyn.js
@@ -1,13 +1,14 @@
 #! /usr/bin/env node
 const { spawn } = require('child_process')
 
-const rimraf = './node_modules/.bin/rimraf .cache lib dist'
+// const rimraf = './node_modules/.bin/rimraf .cache lib dist'
 
 // const parcelBuild = './node_modules/.bin/parcel build src/_client/client.js --no-source-maps -d dist/js -o bundle.js'
 
 // const parcelWatch = './node_modules/.bin/parcel watch src/_client/client.js --public-url . -d dist/js -o bundle.js'
 
-// const eslint = './node_modules/.bin/eslint src'
+const eslint = './node_modules/.bin/eslint src -c ./node_modules/@sharyn/config/.eslintrc.json'
+
 // const flow = './node_modules/.bin/flow'
 // const madge = './node_modules/.bin/madge --circular src'
 
@@ -15,4 +16,4 @@ const rimraf = './node_modules/.bin/rimraf .cache lib dist'
 
 // const jest = './node_modules/.bin/jest --coverage'
 
-spawn(rimraf, { shell: true, stdio: 'inherit' })
+spawn(eslint, { shell: true, stdio: 'inherit' })
---
+++ packages/sharyn-config/README.md
@@ -0,0 +1,13 @@
+# @sharyn/config
+
+## Install
+
+```bash
+yarn add @sharyn-config
+```
+
+## Usage
+
+- Automatic with `@sharyn/cli`
+
+- Run ESLint with the following option: `eslint -c node_modules/@sharyn-config/.eslintrc.json`
