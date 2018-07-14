b2f76aab3b5d27c1c087a79a76495db90ee34a61
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2018-07-14 10:19:03 +0300

    Rewrite of cli script

--- packages/cli/bin.js
+++ packages/cli/bin.js
@@ -1,118 +1,106 @@
 #! /usr/bin/env node
 const { spawn } = require('child_process')
 const fs = require('fs')
+const { swit } = require('@verekia/lib-lang')
+
+const {
+  lintTask,
+  testTask,
+  lintTestTask,
+  prodLocalWithDockerTask,
+  prodLocalWithoutDockerTask,
+} = require('./commands')
+
+const processes = [process]
+const bf = undefined
 
 const mySpawn = cmd => spawn(cmd, { shell: true, stdio: 'inherit' })
 
-let command
-let multiPartCommand = false
-const scriptName = process.argv[2]
+const hasDocker = fs.existsSync(`${process.cwd()}/docker-compose.yml`)
 
-const nodemonCommand = './node_modules/.bin/nodemon -w src -i dist src/_server/require-hook.js'
+// const runLocalSetupThenServer = (serverCommand, runClientWatch = true) => {
+//   multiPartCommand = true
+//   if (fs.existsSync(`${process.cwd()}/docker-compose.yml`)) {
+//     const firstSpawn = mySpawn(localServerSetup)
+//     firstSpawn.on('close', code => {
+//       if (code === 0) {
+//         const serverSpawn = mySpawn(serverCommand)
+//         let clientSpawn
+//         serverSpawn.on('exit', () => {
+//           process.exit(0)
+//           if (clientSpawn) {
+//             clientSpawn.exit(0)
+//           }
+//         })
+//         if (runClientWatch) {
+//           clientSpawn = mySpawn([rmDistCache, clientWatch].join(' && '))
+//         }
+//       }
+//     })
+//   } else {
+//     const serverSpawn = mySpawn(serverCommand)
+//     let clientSpawn
+//     serverSpawn.on('exit', () => {
+//       process.exit(0)
+//       if (clientSpawn) {
+//         clientSpawn.exit(0)
+//       }
+//     })
+//     if (runClientWatch) {
+//       clientSpawn = mySpawn([rmDistCache, clientWatch].join(' && '))
+//     }
+//   }
+// }
 
-const clientWatch = './node_modules/.bin/webpack --mode=development --watch --progress'
+const taskName = process.argv[2]
 
-const rmLib = './node_modules/.bin/rimraf lib'
-const rmDist = './node_modules/.bin/rimraf dist'
-const rmCache = './node_modules/.bin/rimraf .cache' // For Parcel
-const rmDistCache = [rmDist, rmCache].join(' && ')
-
-const eslint = './node_modules/.bin/eslint src'
-const flow = './node_modules/.bin/flow'
-const madge = './node_modules/.bin/madge --circular src'
-const lint = [eslint, flow, madge].join(' && ')
-const test = './node_modules/.bin/jest --coverage'
-
-const dockerUp = 'docker-compose up -d'
-const dockerWaitPg =
-  'until docker run --rm --link db:pg --net js-stack-net postgres:latest pg_isready -U postgres -h pg; do sleep 1; done'
-
-const dbMigr = './node_modules/.bin/knex --knexfile src/_db/knex-config.js --cwd . migrate:latest'
-const dbSeed = './node_modules/.bin/knex --knexfile src/_db/knex-config.js --cwd . seed:run'
-
-const clientBuild = './node_modules/.bin/webpack --mode=production --progress'
-const babel = './node_modules/.bin/babel src -d lib --ignore "**/*.test.js"'
-const prodBuild = [rmDistCache, rmLib, clientBuild, babel].join(' && ')
-
-const localServerSetup = [dockerUp, dockerWaitPg, dbMigr, dbSeed].join(' && ')
-
-const runLocalSetupThenServer = (serverCommand, runClientWatch = true) => {
-  multiPartCommand = true
-  if (fs.existsSync(`${process.cwd()}/docker-compose.yml`)) {
-    const firstSpawn = mySpawn(localServerSetup)
-    firstSpawn.on('close', code => {
-      if (code === 0) {
-        const serverSpawn = mySpawn(serverCommand)
-        let clientSpawn
-        serverSpawn.on('exit', () => {
-          process.exit(0)
-          if (clientSpawn) {
-            clientSpawn.exit(0)
-          }
-        })
-        if (runClientWatch) {
-          clientSpawn = mySpawn([rmDistCache, clientWatch].join(' && '))
-        }
-      }
-    })
-  } else {
-    const serverSpawn = mySpawn(serverCommand)
-    let clientSpawn
-    serverSpawn.on('exit', () => {
-      process.exit(0)
-      if (clientSpawn) {
-        clientSpawn.exit(0)
-      }
-    })
-    if (runClientWatch) {
-      clientSpawn = mySpawn([rmDistCache, clientWatch].join(' && '))
-    }
-  }
-}
-
-switch (scriptName) {
-  case 'dev': {
-    runLocalSetupThenServer(nodemonCommand)
-    break
-  }
-  case 'dev-server-only': {
-    runLocalSetupThenServer(
-      `./node_modules/.bin/cross-env USE_CLIENT_BUNDLE=false ${nodemonCommand}`,
-      false,
-    )
-    break
-  }
-  case 'dev-client-only': {
-    runLocalSetupThenServer(`./node_modules/.bin/cross-env ENABLE_SSR=false ${nodemonCommand}`)
-    break
-  }
-  case 'prod-local': {
-    let commands = []
-    if (fs.existsSync(`${process.cwd()}/docker-compose.yml`)) {
-      commands = [localServerSetup]
-    }
-    const herokuLocal = 'cross-env NODE_ENV=production heroku local'
-    command = commands.concat([prodBuild, herokuLocal]).join(' && ')
-    break
-  }
-  case 'lint': {
-    command = lint
-    break
-  }
-  case 'test': {
-    command = test
-    break
-  }
-  case 'lint-test': {
-    command = [lint, test].join(' && ')
-    break
-  }
-  default:
+swit(
+  taskName,
+  [
+    ['dev', () => {}],
+    ['dev-server-only', () => {}],
+    ['dev-client-only', () => {}],
+    [
+      'prod-local',
+      () =>
+        processes.push(
+          mySpawn(hasDocker ? prodLocalWithDockerTask(bf) : prodLocalWithoutDockerTask(bf)),
+        ),
+    ],
+    ['lint', () => processes.push(mySpawn(lintTask(bf)))],
+    ['test', () => processes.push(mySpawn(testTask(bf)))],
+    ['lint-test', () => processes.push(mySpawn(lintTestTask(bf)))],
+  ],
+  () => {
     // eslint-disable-next-line no-console
-    console.error(`${process.argv[2]} is not a valid @sharyn/cli command.`)
+    console.error(`${taskName} is not a valid @sharyn/cli command.`)
     process.exit(1)
-}
+  },
+)
 
-if (!multiPartCommand) {
-  mySpawn(command)
-}
+// switch (scriptName) {
+//   case 'dev': {
+//     runLocalSetupThenServer(nodemonCommand)
+//     break
+//   }
+//   case 'dev-server-only': {
+//     runLocalSetupThenServer(
+//       `./node_modules/.bin/cross-env USE_CLIENT_BUNDLE=false ${nodemonCommand}`,
+//       false,
+//     )
+//     break
+//   }
+//   case 'dev-client-only': {
+//     runLocalSetupThenServer(`./node_modules/.bin/cross-env ENABLE_SSR=false ${nodemonCommand}`)
+//     break
+//   }
+//   default:
+// }
+
+processes.forEach(p => {
+  p.on('exit', () => {
+    processes.forEach(_p => {
+      _p.exit(0)
+    })
+  })
+})
---
+++ packages/cli/commands.js
@@ -0,0 +1,52 @@
+export const prefix = (command, binFolder = './node_modules/.bin/') => `${binFolder}${command}`
+
+const serverWatch = bf => prefix('nodemon -w src -i dist src/_server/require-hook.js', bf)
+const clientWatch = bf => prefix('webpack --mode=development --watch --progress', bf)
+const clientBuild = bf => prefix('webpack --mode=production --progress', bf)
+
+const rmLib = bf => prefix('rimraf lib', bf)
+const rmDist = bf => prefix('rimraf dist', bf)
+// export const rmParcelCache = bf => prefix('rimraf .cache', bf)
+
+const lint = bf => prefix('eslint src', bf)
+const typecheck = bf => prefix('flow', bf)
+const circular = bf => prefix('madge --circular src', bf)
+const test = bf => prefix('jest --coverage', bf)
+
+const DOCKER_UP = 'docker-compose up -d'
+const DOCKER_WAIT_PG =
+  'until docker run --rm --link db:pg --net sharyn-net postgres:latest pg_isready -U postgres -h pg; do sleep 1; done'
+
+const herokuLocal = bf => prefix('cross-env NODE_ENV=production heroku local', bf)
+
+const dbMigr = bf => prefix('knex --knexfile src/_db/knex-config.js --cwd . migrate:latest', bf)
+const dbSeed = bf => prefix('knex --knexfile src/_db/knex-config.js --cwd . seed:run', bf)
+
+const babel = bf => prefix('babel src -d lib --ignore "**/*.test.js"', bf)
+
+const sequence = arr => arr.join(' && ')
+
+// Combinations
+
+const docker = sequence([DOCKER_UP, DOCKER_WAIT_PG])
+const db = bf => sequence([dbMigr(bf), dbSeed(bf)])
+const localServerSetup = bf => sequence([docker, db(bf)])
+const prodBuild = bf => sequence([rmDist(bf), rmLib(bf), clientBuild(bf), babel(bf)])
+
+// High-level tasks
+
+// export const rmDistParcelCacheTask = bf => sequence([rmDist(bf), rmParcelCache(bf)])
+const lintTask = bf => sequence([lint(bf), typecheck(bf), circular(bf)])
+const lintTestTask = bf => sequence([lintTask(bf), test(bf)])
+const testTask = test
+const prodLocalWithDockerTask = bf =>
+  sequence([localServerSetup(bf), prodBuild(bf), herokuLocal(bf)])
+const prodLocalWithoutDockerTask = bf => sequence([prodBuild(bf), herokuLocal(bf)])
+
+module.exports = {
+  lintTask,
+  lintTestTask,
+  testTask,
+  prodLocalWithDockerTask,
+  prodLocalWithoutDockerTask,
+}
