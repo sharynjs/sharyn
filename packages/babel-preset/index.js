832a4cb364cb301a1ddc752322f311b887402a75
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2018-07-14 20:51:12 +0300

    babel-preset 1.1.0: Add flowtype transform prop types

--- packages/babel-preset/index.js
+++ packages/babel-preset/index.js
@@ -1,4 +1,9 @@
 module.exports = () => ({
   presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-flow'],
   plugins: [['module-resolver', { root: ['./src'] }]],
+  env: {
+    development: {
+      plugins: ['flow-react-proptypes'],
+    },
+  },
 })
