ff6d6931505b894d802f70477b63c1d4ea63b306
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2018-07-17 14:53:04 +0300

    Add test on NODE_ENV to use TEST_DATABASE_URL in sharyn/db knex-config

--- packages/db/knex-config.js
+++ packages/db/knex-config.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
-const { DATABASE_URL } = require('@sharyn/env')
+const { NODE_ENV, DATABASE_URL, TEST_DATABASE_URL } = require('@sharyn/env')
 
 const config = {
   client: 'pg',
-  connection: DATABASE_URL,
+  connection: NODE_ENV === 'test' ? TEST_DATABASE_URL : DATABASE_URL,
   migrations: { directory: './src/_db/migrations' },
 }
 
