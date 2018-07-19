f7fdaffe9215875dd08561ede55e636c82cc849e
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2018-07-19 20:08:28 +0300

    Remove IS_PROD

--- packages/env/index.js
+++ packages/env/index.js
@@ -3,7 +3,4 @@
 require('dotenv/config')
 const readEnv = require('read-env').default
 
-const env = readEnv({ transformKey: false })
-env.IS_PROD = env.NODE_ENV === 'production'
-
-module.exports = env
+module.exports = readEnv({ transformKey: false })
