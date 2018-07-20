ab69074f5ce9fafaf2de85f7dabf8f43bc8369a0
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2018-07-20 18:54:37 +0300

    Make babel-preset peer-agnostic

--- packages/babel-preset/index.js
+++ packages/babel-preset/index.js
@@ -1,15 +1,53 @@
-module.exports = () => ({
-  presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-flow'],
-  plugins: [
-    ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
-    '@babel/plugin-proposal-do-expressions',
-    '@babel/plugin-proposal-nullish-coalescing-operator',
-    '@babel/plugin-proposal-optional-chaining',
-    ['module-resolver', { root: ['./src'] }],
-  ],
+const { hasPackage } = require('@sharyn/check-setup')
+
+const config = {
+  presets: [],
+  plugins: [],
   env: {
     development: {
-      plugins: ['flow-react-proptypes', 'react-hot-loader/babel'],
+      plugins: [],
     },
   },
-})
+}
+
+if (hasPackage('@babel/preset-env')) {
+  config.presets.push('@babel/preset-env')
+}
+
+if (hasPackage('@babel/preset-react')) {
+  config.presets.push('@babel/preset-react')
+}
+
+if (hasPackage('@babel/preset-flow')) {
+  config.presets.push('@babel/preset-flow')
+}
+
+if (hasPackage('@babel/plugin-proposal-pipeline-operator')) {
+  config.plugins.push(['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }])
+}
+
+if (hasPackage('@babel/plugin-proposal-do-expressions')) {
+  config.plugins.push('@babel/plugin-proposal-do-expressions')
+}
+
+if (hasPackage('@babel/plugin-proposal-nullish-coalescing-operator')) {
+  config.plugins.push('@babel/plugin-proposal-nullish-coalescing-operator')
+}
+
+if (hasPackage('@babel/plugin-proposal-optional-chaining')) {
+  config.plugins.push('@babel/plugin-proposal-optional-chaining')
+}
+
+if (hasPackage('babel-plugin-module-resolver')) {
+  config.plugins.push(['module-resolver', { root: ['./src'] }])
+}
+
+if (hasPackage('babel-plugin-flow-react-proptypes')) {
+  config.env.development.plugins.push('flow-react-proptypes')
+}
+
+if (hasPackage('react-hot-loader')) {
+  config.env.development.plugins.push('react-hot-loader/babel')
+}
+
+module.exports = () => config
