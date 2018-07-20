636fa6b2a3e6d7939d75421cb8fcba5966774797
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2018-07-20 16:26:32 +0300

    Fix underline of DrawerItem

--- packages/material-ui/src/DrawerItem/DrawerItem.js
+++ packages/material-ui/src/DrawerItem/DrawerItem.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemIcon from '@material-ui/core/ListItemIcon'
 import ListItemText from '@material-ui/core/ListItemText'
 
-const DrawerItem = ({ label, icon: Icon }) => (
+const styles = { label: { textDecoration: 'none', display: 'inline-block' } }
+
+const DrawerItem = ({ classes, label, icon: Icon }) => (
   <ListItem button>
     {Icon && (
       <ListItemIcon>
         <Icon />
       </ListItemIcon>
     )}
-    <ListItemText primary={label} />
+    <ListItemText primary={label} className={classes.label} />
   </ListItem>
 )
 
 DrawerItem.propTypes = {
+  classes: PropTypes.shape({ label: PropTypes.string }).isRequired,
   label: PropTypes.string.isRequired,
   icon: PropTypes.func,
 }
@@ -24,4 +28,4 @@ DrawerItem.defaultProps = {
   icon: null,
 }
 
-export default DrawerItem
+export default withStyles(styles)(DrawerItem)
