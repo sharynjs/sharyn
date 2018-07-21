8041d4c1159c21b645ea75a5943f5db5af4b77ab
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2018-07-21 21:30:58 +0300

    Default NODE_ENV to development

--- packages/env/index.js
+++ packages/env/index.js
@@ -1,5 +1,9 @@
 /* eslint-disable global-require, import/no-unresolved */
 
+if (!process.env.NODE_ENV) {
+  process.env.NODE_ENV = 'development'
+}
+
 require('dotenv/config')
 const { hasFile } = require('@sharyn/check-setup')
 const readEnv = require('read-env').default
