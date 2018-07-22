6b9645febe69ac616c314b5d9e3dfce6691ce3bc
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2018-07-22 17:50:29 +0300

    Add @babel/runtime to published packages

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-flow', '@babel/preset-react'],
   plugins: [
+    '@babel/plugin-transform-runtime',
     ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
     '@babel/plugin-proposal-do-expressions',
     '@babel/plugin-proposal-nullish-coalescing-operator',
