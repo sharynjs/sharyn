78dc0dc4261c0b3ac8ef2ae71c758c79ce4218ce
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2018-07-22 17:11:23 +0300

    Remove custom ESLint config, tighter syntax for babel-preset

--- packages/babel-preset/src/index.js
+++ packages/babel-preset/src/index.js
@@ -10,44 +10,25 @@ const config = {
   },
 }
 
-if (hasPackage('@babel/preset-env')) {
-  config.presets.push('@babel/preset-env')
-}
+hasPackage('@babel/preset-env') && config.presets.push('@babel/preset-env')
+hasPackage('@babel/preset-react') && config.presets.push('@babel/preset-react')
+hasPackage('@babel/preset-flow') && config.presets.push('@babel/preset-flow')
 
-if (hasPackage('@babel/preset-react')) {
-  config.presets.push('@babel/preset-react')
-}
-
-if (hasPackage('@babel/preset-flow')) {
-  config.presets.push('@babel/preset-flow')
-}
-
-if (hasPackage('@babel/plugin-proposal-pipeline-operator')) {
+hasPackage('@babel/plugin-proposal-pipeline-operator') &&
   config.plugins.push(['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }])
-}
-
-if (hasPackage('@babel/plugin-proposal-do-expressions')) {
+hasPackage('@babel/plugin-proposal-do-expressions') &&
   config.plugins.push('@babel/plugin-proposal-do-expressions')
-}
-
-if (hasPackage('@babel/plugin-proposal-nullish-coalescing-operator')) {
+hasPackage('@babel/plugin-proposal-nullish-coalescing-operator') &&
   config.plugins.push('@babel/plugin-proposal-nullish-coalescing-operator')
-}
-
-if (hasPackage('@babel/plugin-proposal-optional-chaining')) {
+hasPackage('@babel/plugin-proposal-optional-chaining') &&
   config.plugins.push('@babel/plugin-proposal-optional-chaining')
-}
 
-if (hasPackage('babel-plugin-module-resolver')) {
+hasPackage('babel-plugin-module-resolver') &&
   config.plugins.push(['babel-plugin-module-resolver', { root: ['./src'] }])
-}
 
-if (hasPackage('babel-plugin-flow-react-proptypes')) {
+hasPackage('babel-plugin-flow-react-proptypes') &&
   config.env.development.plugins.push('babel-plugin-flow-react-proptypes')
-}
 
-if (hasPackage('react-hot-loader')) {
-  config.env.development.plugins.push('react-hot-loader/babel')
-}
+hasPackage('react-hot-loader') && config.env.development.plugins.push('react-hot-loader/babel')
 
 module.exports = () => config
