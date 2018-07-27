74c6d96429b1f98d4240bba4d84d6d134c71350c
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2018-07-27 23:51:27 +0300

    Remove loggedIn filtering in findMatchAndRoute

--- packages/shared/src/find-match-and-route.js
+++ packages/shared/src/find-match-and-route.js
@@ -3,19 +3,17 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
 import { matchPath } from 'react-router-dom'
-import loggedFilter from './logged-filter'
 
-type Route = { path?: string, loggedInOnly?: boolean, loggedOutOnly?: boolean }
+type Route = { path?: string }
 
-const findMatchAndRoute = (routes: Route[], pathname: string, isLoggedIn: boolean) => {
-  const filteredRoutes = routes.filter(r => loggedFilter(r, isLoggedIn))
-  const notFoundRoute = filteredRoutes.find(r => !r.path)
+const findMatchAndRoute = (routes: Route[], pathname: string) => {
+  const notFoundRoute = routes.find(r => !r.path)
   if (!notFoundRoute) {
     throw Error('You need a route without path to render a 404 page')
   }
   let match
   const activeRoute: Object =
-    filteredRoutes.find(route => {
+    routes.find(route => {
       match = matchPath(pathname, route)
       return match
     }) || notFoundRoute
