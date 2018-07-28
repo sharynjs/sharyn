8f0d14c193ca384f15b110de3259f937f7747659
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2018-07-28 08:55:29 +0300

    Lerna bump comments in npmignore files

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,9 @@
 // so cannot use @sharyn/babel-preset for the sharyn monorepo
 
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react'],
+  presets: ['@babel/preset-env', '@babel/preset-flow', '@babel/preset-react'],
   plugins: [
     '@babel/plugin-transform-runtime',
-    '@babel/plugin-transform-flow-comments',
     ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
     '@babel/plugin-proposal-do-expressions',
     '@babel/plugin-proposal-nullish-coalescing-operator',
