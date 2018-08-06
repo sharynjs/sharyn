03fd78c64d47d915d40566c3a9b0f8846c894d22
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2018-08-06 22:00:09 +0200

    WIP - Bump to Babel 56 but without babel-preset test

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,7 @@
 module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-flow', '@babel/preset-react'],
   plugins: [
-    '@babel/plugin-transform-runtime',
+    ['@babel/plugin-transform-runtime', { corejs: 2 }],
     ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
     '@babel/plugin-proposal-do-expressions',
     '@babel/plugin-proposal-nullish-coalescing-operator',
