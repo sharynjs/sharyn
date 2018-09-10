0e3f5043a7faa540267fb5bddd4e5266f4cc5bf1
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2018-09-10 22:02:43 +0900

    New async reductions

--- packages/client/src/async-reductions.js
+++ packages/client/src/async-reductions.js
@@ -6,15 +6,40 @@ export const REQUEST = 'REQUEST'
 export const SUCCESS = 'SUCCESS'
 export const FAILURE = 'FAILURE'
 
-export const setAsyncEntry = curryRight((asyncState: Object, key: string, value: string) => ({
-  ...asyncState,
-  [key]: value,
-}))
+const del = (state, key) => {
+  const newState = { ...state }
+  delete newState[key]
+  return newState
+}
 
-export const delAsyncEntry = curryRight((asyncState: Object, key: string) => {
-  const newAsyncState = { ...asyncState }
-  delete newAsyncState[key]
-  return newAsyncState
+export const setAsyncRequest = curryRight((asyncState: Object, params: Object) => {
+  if (params.useStatus === false && params.status) {
+    throw Error('setAsyncRequest cannot have both a status option and a useStatus set to false')
+  }
+  return {
+    ...asyncState,
+    [params.key]: params.useStatus || params.status ? params.status ?? REQUEST : true,
+  }
 })
+
+export const setAsyncSuccess = curryRight((asyncState: Object, params: Object) => {
+  if (params.useStatus === false && params.status) {
+    throw Error('setAsyncSuccess cannot have both a status option and a useStatus set to false')
+  }
+  return params.useStatus || params.status
+    ? { ...asyncState, [params.key]: params.status ?? SUCCESS }
+    : del(asyncState, params.key)
+})
+
+export const setAsyncFailure = curryRight((asyncState: Object, params: Object) => {
+  if (params.useStatus === false && params.status) {
+    throw Error('setAsyncFailure cannot have both a status option and a useStatus set to false')
+  }
+  return params.useStatus || params.status
+    ? { ...asyncState, [params.key]: params.status ?? FAILURE }
+    : del(asyncState, params.key)
+})
+
+export const delAsyncEntry = curryRight((asyncState: Object, key: string) => del(asyncState, key))
 
 export const clearAsync = () => ({})
--- packages/client/src/async-reductions.test.js
+++ packages/client/src/async-reductions.test.js
@@ -1,10 +1,49 @@
 import { compose } from 'recompose'
-import { setAsyncEntry, delAsyncEntry, clearAsync } from './async-reductions'
+import {
+  setAsyncRequest,
+  setAsyncSuccess,
+  setAsyncFailure,
+  delAsyncEntry,
+  clearAsync,
+} from './async-reductions'
 
-test('setAsyncEntry', () => {
-  const asyncState = { foo: 'foo' }
-  expect(setAsyncEntry(asyncState, 'bar', 'BAR')).toEqual({ foo: 'foo', bar: 'BAR' })
-  expect(compose(setAsyncEntry('baz', 'BAZ'))(asyncState)).toEqual({ foo: 'foo', baz: 'BAZ' })
+test('setAsyncRequest', () => {
+  const asyncState = {}
+  expect(setAsyncRequest(asyncState, { key: 'foo' })).toEqual({ foo: true })
+  expect(setAsyncRequest(asyncState, { key: 'foo', useStatus: true })).toEqual({ foo: 'REQUEST' })
+  expect(setAsyncRequest(asyncState, { key: 'foo', status: 'CUSTOM' })).toEqual({ foo: 'CUSTOM' })
+  expect(() =>
+    setAsyncRequest(asyncState, { key: 'foo', useStatus: false, status: 'CUSTOM' }),
+  ).toThrow()
+  expect(compose(setAsyncRequest({ key: 'foo', status: 'CUSTOM' }))(asyncState)).toEqual({
+    foo: 'CUSTOM',
+  })
+})
+
+test('setAsyncSuccess', () => {
+  const asyncState = { foo: true }
+  expect(setAsyncSuccess(asyncState, { key: 'foo' })).toEqual({})
+  expect(setAsyncSuccess(asyncState, { key: 'foo', useStatus: true })).toEqual({ foo: 'SUCCESS' })
+  expect(setAsyncSuccess(asyncState, { key: 'foo', status: 'CUSTOM' })).toEqual({ foo: 'CUSTOM' })
+  expect(() =>
+    setAsyncSuccess(asyncState, { key: 'foo', useStatus: false, status: 'CUSTOM' }),
+  ).toThrow()
+  expect(compose(setAsyncSuccess({ key: 'foo', status: 'CUSTOM' }))(asyncState)).toEqual({
+    foo: 'CUSTOM',
+  })
+})
+
+test('setAsyncFailure', () => {
+  const asyncState = { foo: true }
+  expect(setAsyncFailure(asyncState, { key: 'foo' })).toEqual({})
+  expect(setAsyncFailure(asyncState, { key: 'foo', useStatus: true })).toEqual({ foo: 'FAILURE' })
+  expect(setAsyncFailure(asyncState, { key: 'foo', status: 'CUSTOM' })).toEqual({ foo: 'CUSTOM' })
+  expect(() =>
+    setAsyncFailure(asyncState, { key: 'foo', useStatus: false, status: 'CUSTOM' }),
+  ).toThrow()
+  expect(compose(setAsyncFailure({ key: 'foo', status: 'CUSTOM' }))(asyncState)).toEqual({
+    foo: 'CUSTOM',
+  })
 })
 
 test('delAsyncEntry', () => {
