5d71ac0ccb3e87b617c6db9be07c4e9fc985350e
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2018-09-16 22:57:51 +0900

    Better prefer-destructuring rule

--- packages/eslint-config/src/index.js
+++ packages/eslint-config/src/index.js
@@ -15,6 +15,13 @@ const config: Object = {
     'no-underscore-dangle': 0,
     'func-names': 0,
     'no-unexpected-multiline': 2,
+    'prefer-destructuring': [
+      2,
+      {
+        VariableDeclarator: { object: true, array: true },
+        AssignmentExpression: { object: false, array: false },
+      },
+    ],
     semi: [2, 'never'],
   },
   settings: {},
