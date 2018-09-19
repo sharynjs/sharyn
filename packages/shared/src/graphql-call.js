dd6e29db48fb5b2b182186bb5c86fc90f4e6c91d
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2018-09-20 03:39:18 +0900

    Add mapFields support to graphqlCall

--- packages/shared/src/graphql-call.js
+++ packages/shared/src/graphql-call.js
@@ -7,20 +7,27 @@ import call from './call'
 const graphqlCall = async ({
   urlBase,
   query,
-  urlParams,
+  urlParams = {},
   mapUrlParams,
+  fields = {},
+  mapFields,
   mapResp,
   cookie,
 }: {
   urlBase?: string,
   query: string,
-  urlParams: Object,
+  urlParams?: Object,
   mapUrlParams?: Function,
+  fields?: Object,
+  mapFields?: Function,
   mapResp?: Function,
   cookie?: string,
 }) => {
   let callResp
-  const variables = mapUrlParams ? mapUrlParams(urlParams) : urlParams
+  const variables = {
+    ...(mapUrlParams ? mapUrlParams(urlParams) : urlParams),
+    ...(mapFields ? mapFields(fields) : fields),
+  }
   try {
     callResp = await call({ urlBase, cookie, body: { query, variables } })
   } catch (err) {
