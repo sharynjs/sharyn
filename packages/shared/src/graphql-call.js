c3dce5bc059ddc506617c1a1d7658985a1ecf7da
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2018-09-20 03:27:06 +0900

    Include mapUrlParams to graphqlCall

--- packages/shared/src/graphql-call.js
+++ packages/shared/src/graphql-call.js
@@ -7,17 +7,20 @@ import call from './call'
 const graphqlCall = async ({
   urlBase,
   query,
-  variables,
+  urlParams,
+  mapUrlParams,
   mapResp,
   cookie,
 }: {
   urlBase?: string,
   query: string,
-  variables: Object,
+  urlParams: Object,
+  mapUrlParams?: Function,
   mapResp?: Function,
   cookie?: string,
 }) => {
   let callResp
+  const variables = mapUrlParams ? mapUrlParams(urlParams) : urlParams
   try {
     callResp = await call({ urlBase, cookie, body: { query, variables } })
   } catch (err) {
