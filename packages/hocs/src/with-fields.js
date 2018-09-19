04174330299156584c9d4e343d43cf05e8edcf9e
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2018-09-19 20:17:58 +0900

    Rename to handleFieldChange and add setField

--- packages/hocs/src/with-fields.js
+++ packages/hocs/src/with-fields.js
@@ -3,12 +3,16 @@
 import compose from 'recompose/compose'
 import withState from 'recompose/withState'
 import withHandlers from 'recompose/withHandlers'
+import withProps from 'recompose/withProps'
 
 const withFields = (initialStateFn?: Function) => (Cmp: Function) =>
   compose(
     withState('fields', 'setFields', initialStateFn || {}),
+    withProps({
+      setField: ({ fields, setFields }) => (key, value) => setFields({ ...fields, [key]: value }),
+    }),
     withHandlers({
-      fieldChangeHandler: ({ fields, setFields }) => ({ target }, checked) => {
+      handleFieldChange: ({ fields, setFields }) => ({ target }, checked) => {
         const newFields = { ...fields }
         if (checked !== undefined ? checked === false : target.value === '') {
           delete newFields[target.name]
