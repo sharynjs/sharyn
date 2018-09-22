1b58574d9c173762bd4d1236ed5581d0cee950f1
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2018-09-22 16:37:13 +0900

    Updated findMatch to work on plain routes instead of routes and components

--- packages/shared/src/find-match.js
+++ packages/shared/src/find-match.js
@@ -6,36 +6,30 @@ import { matchPath } from 'react-router-dom'
 import loggedFilter from './logged-filter'
 
 type Route = { path?: string, loggedOutOnly?: boolean, loggedInOnly?: boolean }
-type RouteAndCmp = { route: Route, component: Function }
 
-const findMatch = (routesAndCmps: RouteAndCmp[], pathname: string, isLoggedIn: boolean) => {
-  const routesAndCmpsWithGoodPaths = routesAndCmps.map(rac => ({
-    ...rac,
-    route: {
-      ...rac.route,
-      path: typeof rac.route.path === 'function' ? rac.route.path() : rac.route.path,
-    },
+const findMatch = (routes: Route[], pathname: string, isLoggedIn: boolean) => {
+  const routesWithPlainPaths: Route[] = routes.map(r => ({
+    ...r,
+    path: typeof r.path === 'function' ? r.path() : r.path,
   }))
-  const filteredRoutesAndCmps = routesAndCmpsWithGoodPaths.filter(({ route }) =>
-    loggedFilter(route, isLoggedIn),
-  )
-  const notFoundRouteAndCmp = filteredRoutesAndCmps.find(({ route }) => !route.path)
-  if (!notFoundRouteAndCmp) {
+  const accessibleRoutes: Route[] = routesWithPlainPaths.filter(r => loggedFilter(r, isLoggedIn))
+  const notFoundRoute = accessibleRoutes.find(({ path }) => !path)
+  if (!notFoundRoute) {
     throw Error('You need a route without path to render a 404 page')
   }
   let match
-  const activeRouteAndCmp: Object =
-    filteredRoutesAndCmps.find(({ route }) => {
+  const activeRoute: Object =
+    accessibleRoutes.find(route => {
       match = matchPath(pathname, route)
       return match
-    }) || notFoundRouteAndCmp
-  const { path, exact, ...route } = activeRouteAndCmp.route // Removing these properties from route
+    }) || notFoundRoute
+  const { path, exact, ...route } = activeRoute // Removing these properties from route
   return {
     match,
     route,
-    Component: activeRouteAndCmp.component,
-    // flow-disable-next-line
-    routes: filteredRoutesAndCmps.map(rac => rac.route),
+    routesWithPlainPaths,
+    accessibleRoutes,
+    notFoundRoute,
   }
 }
 
