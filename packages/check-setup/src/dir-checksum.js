c12f7d73be9784c7471a77e4b33f16692d6835b4
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2018-09-23 23:31:36 +0900

    Remove more paths in dirChecksum

--- packages/check-setup/src/dir-checksum.js
+++ packages/check-setup/src/dir-checksum.js
@@ -1,5 +1,6 @@
 // @flow
 
+const pathModule = require('path')
 const fs = require('fs')
 // flow-disable-next-line
 const klawSync = require('klaw-sync')
@@ -7,9 +8,16 @@ const klawSync = require('klaw-sync')
 const md5 = require('md5')
 
 const dirChecksum = (dirPath: string) => {
+  const basePath = pathModule.resolve(dirPath)
   const files = klawSync(dirPath, {
     nodir: true,
-    filter: ({ path }) => !RegExp('node_modules').test(path) && !RegExp('.git').test(path),
+    filter: ({ path }) =>
+      !RegExp(`^${basePath}/node_modules/`).test(path) &&
+      !RegExp(`^${basePath}/dist/`).test(path) &&
+      !RegExp(`^${basePath}/.git/`).test(path) &&
+      !RegExp(`^${basePath}/coverage/`).test(path) &&
+      !RegExp(`^${basePath}/lib/`).test(path) &&
+      !RegExp(`^${basePath}/data/`).test(path),
   }).map(f => f.path)
   const md5s = files.map(f => fs.readFileSync(f))
   const concatenatedMd5s = md5s.join()
