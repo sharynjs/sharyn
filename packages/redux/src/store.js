f5dc23df9519200c6c6d92bf65bc7de7ee956d29
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2018-09-25 22:22:48 +0900

    Add persistUser option to sharynStore

--- packages/redux/src/store.js
+++ packages/redux/src/store.js
@@ -4,6 +4,8 @@
 import { applyMiddleware, combineReducers, compose, createStore } from 'redux'
 // flow-disable-next-line
 import thunk from 'redux-thunk'
+// flow-disable-next-line
+import persistState from 'redux-localstorage'
 
 import asyncReducer from './async-reducer'
 import envReducer from './env-reducer'
@@ -11,10 +13,17 @@ import dataReducer from './data-reducer'
 import uiReducer from './ui-reducer'
 import userReducer from './user-reducer'
 
-const createSharynStore = (options?: { preloadedState?: Object, isDevEnv?: boolean }) => {
+const createSharynStore = (options?: {
+  preloadedState?: Object,
+  isDevEnv?: boolean,
+  persistUser?: boolean,
+}) => {
   const composeEnhancers =
     (options?.isDevEnv && window?.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
-  const composedEnhancers = composeEnhancers(applyMiddleware(thunk))
+  const composedEnhancers = composeEnhancers(
+    applyMiddleware(thunk),
+    options?.persistUser ? persistState('user') : undefined,
+  )
 
   return createStore(
     combineReducers({
