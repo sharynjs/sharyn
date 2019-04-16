e64cc7f5763a669351562acbc6f601f23d2af0cc
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2019-04-16 13:49:04 +0700

    Add match to getPageInfo

--- packages/shared/src/get-page-info.js
+++ packages/shared/src/get-page-info.js
@@ -1,12 +1,13 @@
 // @flow
 
-const getPageInfo = (route: Object, state: Object) => {
+const getPageInfo = (route: Object, state: Object, match: Object) => {
   const pageInfo = {}
   if (route.title) {
-    pageInfo.title = typeof route.title === 'function' ? route.title(state) : route.title
+    pageInfo.title = typeof route.title === 'function' ? route.title(state, match) : route.title
   }
   if (route.backNav) {
-    pageInfo.backNav = typeof route.backNav === 'function' ? route.backNav(state) : route.backNav
+    pageInfo.backNav =
+      typeof route.backNav === 'function' ? route.backNav(state, match) : route.backNav
   }
   return pageInfo
 }
