957891954a319555cb2f819adff4bb1a612c081e
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2019-05-07 17:46:57 +0700

    Fix babel-preset with a static config

--- src/babel-preset/index.js
+++ src/babel-preset/index.js
@@ -1,41 +1,19 @@
 // @flow
 
-import { hasPackage } from '../check-setup'
-
 const config = {
-  presets: [],
-  plugins: [],
+  presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-flow'],
+  plugins: [
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    '@babel/plugin-proposal-optional-catch-binding',
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-syntax-dynamic-import',
+    ['babel-plugin-module-resolver', { root: ['./src'] }],
+  ],
   env: {
     development: {
-      plugins: [],
+      plugins: ['babel-plugin-flow-react-proptypes', 'react-hot-loader/babel'],
     },
   },
 }
-
-hasPackage('@babel/preset-env') && config.presets.push('@babel/preset-env')
-hasPackage('@babel/preset-react') && config.presets.push('@babel/preset-react')
-hasPackage('@babel/preset-flow') && config.presets.push('@babel/preset-flow')
-
-hasPackage('@babel/plugin-proposal-pipeline-operator') &&
-  config.plugins.push(['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }])
-hasPackage('@babel/plugin-proposal-do-expressions') &&
-  config.plugins.push('@babel/plugin-proposal-do-expressions')
-hasPackage('@babel/plugin-proposal-nullish-coalescing-operator') &&
-  config.plugins.push('@babel/plugin-proposal-nullish-coalescing-operator')
-hasPackage('@babel/plugin-proposal-optional-chaining') &&
-  config.plugins.push('@babel/plugin-proposal-optional-chaining')
-hasPackage('@babel/plugin-proposal-optional-catch-binding') &&
-  config.plugins.push('@babel/plugin-proposal-optional-catch-binding')
-
-hasPackage('@babel/plugin-syntax-dynamic-import') &&
-  config.plugins.push('@babel/plugin-syntax-dynamic-import')
-
-hasPackage('babel-plugin-module-resolver') &&
-  config.plugins.push(['babel-plugin-module-resolver', { root: ['./src'] }])
-
-hasPackage('babel-plugin-flow-react-proptypes') &&
-  config.env.development.plugins.push('babel-plugin-flow-react-proptypes')
-
-hasPackage('react-hot-loader') && config.env.development.plugins.push('react-hot-loader/babel')
 
 module.exports = () => config
