2cf748b92061de040d28056c6efb51b0dfbbc78c
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2019-06-27 14:35:39 +0200

    Fix getPageInfo falsey backNav

--- packages/shared/src/get-page-info.js
+++ packages/shared/src/get-page-info.js
@@ -2,10 +2,10 @@
 
 const getPageInfo = (route: Object, state: Object, match: Object) => {
   const pageInfo = {}
-  if (route.title) {
+  if (route.title !== undefined) {
     pageInfo.title = typeof route.title === 'function' ? route.title(state, match) : route.title
   }
-  if (route.backNav) {
+  if (route.backNav !== undefined) {
     pageInfo.backNav =
       typeof route.backNav === 'function' ? route.backNav(state, match) : route.backNav
   }
