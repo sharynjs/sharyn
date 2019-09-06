a384cf07581f5e4df05afedd4112123e5c71e11d
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2019-09-06 20:58:10 +0900

    Isolated getFormData

---
+++ browser/getFormData/index.js
@@ -0,0 +1,12 @@
+const getFormData = (formElement, onlyTruthy) => {
+  const formData = new FormData(formElement)
+  const data = {}
+  Array.from(formData.entries()).forEach(([key, value]) => {
+    if (!onlyTruthy || value) {
+      data[key] = value
+    }
+  })
+  return data
+}
+
+module.exports = getFormData
