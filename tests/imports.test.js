6bb4defd6e0eb13f2cf8dbd4ca70e4f5c8842e22
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2019-09-09 21:57:28 +0900

    Add tests for global

--- tests/imports.test.js
+++ tests/imports.test.js
@@ -56,57 +56,63 @@ test('imports', () => {
         'tryCatch',
         'wait',
       ],
-      properties: ['getGlobal', 'setGlobal', 'getAllGlobal', 'deleteGlobal', 'clearGlobal'],
+      multipleExportsModules: [
+        {
+          name: 'global',
+          exportNames: ['getGlobal', 'setGlobal', 'getAllGlobal', 'deleteGlobal', 'clearGlobal'],
+        },
+      ],
     },
-  ].forEach(({ groupName, onSharynRoot, modules, properties, isOnSharynDev }) => {
-    const group = require(`@sharyn/${groupName}`)
-    const sharynGroup = require(`sharyn${isOnSharynDev ? '-dev' : ''}/${groupName}`)
-    const sharyn = isOnSharynDev ? _sharynDev : _sharyn
+  ].forEach(
+    ({ groupName, onSharynRoot, modules, properties, isOnSharynDev, multipleExportsModules }) => {
+      const group = require(`@sharyn/${groupName}`)
+      const sharynGroup = require(`sharyn${isOnSharynDev ? '-dev' : ''}/${groupName}`)
+      const sharyn = isOnSharynDev ? _sharynDev : _sharyn
 
-    expect(typeof group).toBe('object')
-    expect(group).toBe(sharynGroup)
+      expect(typeof group).toBe('object')
+      expect(group).toBe(sharynGroup)
 
-    properties &&
-      properties.forEach(propertyName => {
-        expect(group[propertyName]).not.toBe(undefined)
-        expect(group[propertyName]).toBe(sharynGroup[propertyName])
-      })
+      properties &&
+        properties.forEach(propertyName => {
+          expect(group[propertyName]).not.toBe(undefined)
+          expect(group[propertyName]).toBe(sharynGroup[propertyName])
+        })
 
-    modules &&
-      modules.forEach(moduleName => {
-        const individualModule = require(`@sharyn/${groupName}.${moduleName.toLowerCase()}`)
-        const groupModule = require(`@sharyn/${groupName}/${moduleName}`)
-        const sharynModule = require(`sharyn${
-          isOnSharynDev ? '-dev' : ''
-        }/${groupName}/${moduleName}`)
+      modules &&
+        modules.forEach(moduleName => {
+          const individualModule = require(`@sharyn/${groupName}.${moduleName.toLowerCase()}`)
+          const groupModule = require(`@sharyn/${groupName}/${moduleName}`)
+          const sharynModule = require(`sharyn${
+            isOnSharynDev ? '-dev' : ''
+          }/${groupName}/${moduleName}`)
 
-        expect(individualModule).not.toBe(undefined)
-        expect(individualModule).toBe(groupModule)
-        expect(individualModule).toBe(sharynModule)
-        expect(individualModule).toBe(group[moduleName])
-        expect(individualModule).toBe(sharynGroup[moduleName])
-        onSharynRoot && expect(individualModule).toBe(sharyn[moduleName])
-      })
-  })
+          expect(individualModule).not.toBe(undefined)
+          expect(individualModule).toBe(groupModule)
+          expect(individualModule).toBe(sharynModule)
+          expect(individualModule).toBe(group[moduleName])
+          expect(individualModule).toBe(sharynGroup[moduleName])
+          onSharynRoot && expect(individualModule).toBe(sharyn[moduleName])
+        })
 
-  // import glob from '@sharyn/util.global'
-  // import utilGlobal from '@sharyn/util/global'
-  // import sharynUtilGlobal from 'sharyn/util/global'
-  // import { getGlobal as getGlobal1 } from '@sharyn/util'
-  // import { setGlobal as setGlobal1 } from '@sharyn/util'
-  // import { getAllGlobal as getAllGlobal1 } from '@sharyn/util'
-  // import { deleteGlobal as deleteGlobal1 } from '@sharyn/util'
-  // import { clearGlobal as clearGlobal1 } from '@sharyn/util'
-  // import { getGlobal as getGlobal2 } from 'sharyn/util'
-  // import { setGlobal as setGlobal2 } from 'sharyn/util'
-  // import { getAllGlobal as getAllGlobal2 } from 'sharyn/util'
-  // import { deleteGlobal as deleteGlobal2 } from 'sharyn/util'
-  // import { clearGlobal as clearGlobal2 } from 'sharyn/util'
-  // import { getGlobal as getGlobal3 } from 'sharyn'
-  // import { setGlobal as setGlobal3 } from 'sharyn'
-  // import { getAllGlobal as getAllGlobal3 } from 'sharyn'
-  // import { deleteGlobal as deleteGlobal3 } from 'sharyn'
-  // import { clearGlobal as clearGlobal3 } from 'sharyn'
+      multipleExportsModules &&
+        multipleExportsModules.forEach(({ name, exportNames }) => {
+          const individualModule = require(`@sharyn/${groupName}.${name.toLowerCase()}`)
+          const groupModule = require(`@sharyn/${groupName}/${name}`)
+          const sharynModule = require(`sharyn${isOnSharynDev ? '-dev' : ''}/${groupName}/${name}`)
 
-  // test global too
+          expect(individualModule).not.toBe(undefined)
+          expect(individualModule).toBe(groupModule)
+          expect(individualModule).toBe(sharynModule)
+          exportNames.forEach(exportName => {
+            const exp = individualModule[exportName]
+            expect(exp).not.toBe(undefined)
+            expect(exp).toBe(groupModule[exportName])
+            expect(exp).toBe(sharynModule[exportName])
+            expect(exp).toBe(group[exportName])
+            expect(exp).toBe(sharynGroup[exportName])
+            onSharynRoot && expect(exp).toBe(sharyn[exportName])
+          })
+        })
+    }
+  )
 })
