47fe579a12973b173f12faeea684e4dc16ea5020
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2019-10-07 20:07:32 +0900

    Add loading-page data-testid hardcoded on LoadingPage

--- packages/components/src/LoadingPage.js
+++ packages/components/src/LoadingPage.js
@@ -11,15 +11,8 @@ import Progress from '@material-ui/core/CircularProgress'
 import withStyles from '@material-ui/core/styles/withStyles'
 import DelayedProgress from './DelayedProgress'
 
-const LoadingPageJSX = ({
-  classes: css,
-  noDelay,
-  ...rest
-}: {
-  classes: Object,
-  noDelay?: boolean,
-}) => (
-  <div className={css.middle} {...rest}>
+const LoadingPageJSX = ({ classes: css, noDelay }: { classes: Object, noDelay?: boolean }) => (
+  <div className={css.middle} data-testid="loading-page">
     {noDelay ? <Progress thickness={4} size={70} /> : <DelayedProgress thickness={4} size={70} />}
   </div>
 )
