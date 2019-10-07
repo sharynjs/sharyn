3e3dca9b232ca861d25cc02a84c4ae3890da9be0
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2019-10-07 19:55:18 +0900

    Forward rest to div in LoadingPage

--- packages/components/src/LoadingPage.js
+++ packages/components/src/LoadingPage.js
@@ -11,8 +11,15 @@ import Progress from '@material-ui/core/CircularProgress'
 import withStyles from '@material-ui/core/styles/withStyles'
 import DelayedProgress from './DelayedProgress'
 
-const LoadingPageJSX = ({ classes: css, noDelay }: { classes: Object, noDelay?: boolean }) => (
-  <div className={css.middle}>
+const LoadingPageJSX = ({
+  classes: css,
+  noDelay,
+  ...rest
+}: {
+  classes: Object,
+  noDelay?: boolean,
+}) => (
+  <div className={css.middle} {...rest}>
     {noDelay ? <Progress thickness={4} size={70} /> : <DelayedProgress thickness={4} size={70} />}
   </div>
 )
